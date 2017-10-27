2337d113ec34872d28bc719c308ee10155e6ff09
James Roth <jamesroth@gmail.com>
2017-10-27 08:47:09 -0400

    refactor

--- source/mpower.py
+++ source/mpower.py
@@ -22,100 +22,9 @@ import sys
 import datetime
 import xml.etree.cElementTree as ET
 import xml.dom.minidom as minidom
-import physics
-
-import numpy as np
-from scipy import interpolate   
+import mpowertcx.physics
+from mpowertcx.ride import Ride, RideHeader
  
-class RideHeader(object):
-    """ Summary statistics for a ride """
-    def __init__(self):
-        self.setSummary()
-
-    def setSummary(self, time=0, distance=0, average_power=0, max_power=0, average_rpm=0, max_rpm=0, average_hr=0, max_hr=0, calories=0):
-        self.time = time
-        self.distance = distance
-        self.average_power = average_power
-        self.max_power = max_power
-        self.average_rpm = average_rpm
-        self.max_rpm = max_rpm
-        self.average_hr = average_hr
-        self.max_hr = max_hr
-        self.calories = calories
-
-
-class Ride(object):
-    """ Hold the time series data for the ride and a header """
-    def __init__(self):
-        self.power = []
-        self.rpm = []
-        self.hr = []
-        self.distance = []
-        self.header = RideHeader()
-
-    def addSample(self, power=0, rpm=0, hr=0, distance=0):
-        self.power.append(str(power))
-        self.rpm.append(str(rpm))
-        self.hr.append(str(hr))
-        self.distance.append(str(distance))
-
-    def count(self):
-        """ The number of samples in the time series """
-        return len(self.power)
-        
-    def inferHeader(self, time=0):
-        average_power = sum(int(p) for p in self.power) / len(self.power)
-        max_power = max(int(p) for p in self.power)
-        print ("%d %d" % (average_power, max_power))
-        self.header.setSummary(time=time, distance=0, average_power=average_power, max_power=max_power)
-
-    def delta(self):
-        if self.count():
-            return self.header.time / self.count()
-        else:
-            return 0
-        
-    def interpolate(self):
-        seconds = self.header.time
-        delta = self.delta()
-        print ("%.2f seconds per sample before interpolation" % (delta))
-        
-        if delta == 0:
-            print ("nothing to interpolate")
-            return
-        
-        limit = int(seconds)
-        xa = np.arange(0, limit, delta)
-        xb = np.arange(0, limit - delta, 1)
-        
-        #print (limit)
-        #print (xa)
-        #print (xb)
-        
-        f = interpolate.interp1d(xa, self.power)
-        self.power = f(xb).astype("int").astype("str")
-        
-        f = interpolate.interp1d(xa, self.rpm)
-        self.rpm = f(xb).astype("int").astype("str")
-        
-        f = interpolate.interp1d(xa, self.hr)
-        self.hr = f(xb).astype("int").astype("str")
-        
-        f = interpolate.interp1d(xa, self.distance)
-        self.distance = f(xb).astype("str")
-        
-    def modelDistance(self):
-        delta = self.delta()
-        print ("delta %.2f" % delta)
-        bike = physics.SimpleBike()
-        bike.time_delta = delta
-        self.distance = []
-
-        for p in self.power:
-            power, v_mph, distance = bike.next_sample(float(p))
-            self.distance.append(distance)
-            #print (power, v_mph, distance)
-
 
 class LineIterator(object):
     def __init__(self, stream):
@@ -462,10 +371,10 @@ class MPower(object):
     def save_data(self, filename, start_time, model=False):
         """ Save the parsed CSV to TCX """
 
-        self.ride.interpolate()
+        #self.ride.interpolate()
         
         #if model:
-        self.ride.modelDistance()
+        #self.ride.modelDistance()
 
         now = self._format_time(start_time)
         root = ET.Element("TrainingCenterDatabase")
---
+++ source/mpowertcx/ride.py
@@ -0,0 +1,89 @@
+import numpy as np
+from scipy import interpolate   
+
+ 
+class RideHeader(object):
+    """ Summary statistics for a ride """
+    def __init__(self):
+        self.setSummary()
+
+    def setSummary(self, time=0, distance=0, average_power=0, max_power=0, average_rpm=0, max_rpm=0, average_hr=0, max_hr=0, calories=0):
+        self.time = time
+        self.distance = distance
+        self.average_power = average_power
+        self.max_power = max_power
+        self.average_rpm = average_rpm
+        self.max_rpm = max_rpm
+        self.average_hr = average_hr
+        self.max_hr = max_hr
+        self.calories = calories
+
+
+class Ride(object):
+    """ Hold the time series data for the ride and a header """
+    def __init__(self):
+        self.power = []
+        self.rpm = []
+        self.hr = []
+        self.distance = []
+        self.header = RideHeader()
+
+    def addSample(self, power=0, rpm=0, hr=0, distance=0):
+        self.power.append(str(power))
+        self.rpm.append(str(rpm))
+        self.hr.append(str(hr))
+        self.distance.append(str(distance))
+
+    def count(self):
+        """ The number of samples in the time series """
+        return len(self.power)
+        
+    def inferHeader(self, time=0):
+        average_power = sum(int(p) for p in self.power) / len(self.power)
+        max_power = max(int(p) for p in self.power)
+        print ("%d %d" % (average_power, max_power))
+        self.header.setSummary(time=time, distance=0, average_power=average_power, max_power=max_power)
+
+    def delta(self):
+        if self.count():
+            return self.header.time / self.count()
+        else:
+            return 0
+        
+    def interpolate(self):
+        seconds = self.header.time
+        delta = self.delta()
+        print ("%.2f seconds per sample before interpolation" % (delta))
+        
+        if delta == 0:
+            print ("nothing to interpolate")
+            return
+        
+        limit = int(seconds)
+        xa = np.arange(0, limit, delta)
+        xb = np.arange(0, limit - delta, 1)
+       
+        f = interpolate.interp1d(xa, self.power)
+        self.power = f(xb).astype("int").astype("str")
+        
+        f = interpolate.interp1d(xa, self.rpm)
+        self.rpm = f(xb).astype("int").astype("str")
+        
+        f = interpolate.interp1d(xa, self.hr)
+        self.hr = f(xb).astype("int").astype("str")
+        
+        f = interpolate.interp1d(xa, self.distance)
+        self.distance = f(xb).astype("str")
+        
+    def modelDistance(self):
+        delta = self.delta()
+        print ("delta %.2f" % delta)
+        bike = physics.SimpleBike()
+        bike.time_delta = delta
+        self.distance = []
+
+        for p in self.power:
+            power, v_mph, distance = bike.next_sample(float(p))
+            self.distance.append(distance)
+            #print (power, v_mph, distance)
+
