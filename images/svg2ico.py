be413d10a38de45dc6cc51fb0e5e439aba837741
James Roth <jamesroth@gmail.com>
2017-11-21 08:32:24 -0500

    more icon obsession

---
+++ images/svg2ico.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+
+sizes = (16, 32, 48, 64, 96, 128, 256)
+
+svg = sys.argv[1]
+basename, extension = os.path.splitext(svg)
+ico = basename + '.ico'
+tmp_names = []
+
+for size in sizes:
+    png = 'tmp/' + str(size) + '.png'
+    tmp_names.append(png)
+    subprocess.call(['inkscape', '-z', '-e', png, '-w', str(size), '-h', str(size), svg])
+
+#subprocess.call(['convert'] + tmp_names + ['-colors', '512', ico])
+subprocess.call(['convert'] + tmp_names + [ico])
+#	 	 '-dither', 'None', '-colors', '256', ico])
+
