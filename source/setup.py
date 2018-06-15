1ea6e603dc4a0108e2bed37938bfc58335be7b03
James Roth <jamesroth@gmail.com>
2018-06-15 19:19:54 -0400

    bump to version 2.0.4

--- source/setup.py
+++ source/setup.py
@@ -25,7 +25,7 @@ executables = [
 ]
 
 setup(name='MPowerTCX',
-      version = '2.0.3',
+      version = '2.0.4',
       description = '',
       options = options,
       executables = executables)
