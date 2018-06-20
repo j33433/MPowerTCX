366513021875be51900b2e973fd3432b6a6c662c
James Roth <jamesroth@gmail.com>
2018-06-20 19:35:07 -0400

    test with more flags --model and --interpolate

--- source/run_samples.py
+++ source/run_samples.py
@@ -10,17 +10,34 @@ import subprocess
 testdir = '../samples'
 files = os.listdir(testdir)
 
+def tcx_name(filename, testdir, tag, ext):
+    parts = filename.split('.')
+    parts.insert(1, tag)
+    parts[-1] = ext
+    joined = ''.join(parts)
+    full = os.path.join(testdir, joined)
+    
+    return full
+
 for f in files:
     if f.lower().endswith('.csv'):
-        parts = f.split('.')
-        parts[-1] = '.tcx'
-        tcx = ''.join(parts)
         csv_full = os.path.join(testdir, f)
-        tcx_full = os.path.join(testdir, tcx)
 
         # An arbitrary, fixed time for testing.
         timestamp = '2010-10-19T20:56:35.450686'
         print ('run_samples.py: %r' % (csv_full))
-        args = ['./mpowertcx.py', '--csv', csv_full, '--tcx', tcx_full, '--time', timestamp] #, '--model', '70']
+
+        tcx_full = tcx_name(f, testdir, '', '.tcx')
+        args = ['./mpowertcx.py', '--csv', csv_full, '--tcx', tcx_full, '--time', timestamp]
+        print ('run_samples.py: %r' % args)
+        subprocess.call(args)
+
+        tcx_model = tcx_name(f, testdir, '_model', '.tcx')
+        args = ['./mpowertcx.py', '--csv', csv_full, '--tcx', tcx_model, '--time', timestamp, '--model', '70']
+        print ('run_samples.py: %r' % args)
+        subprocess.call(args)
+
+        tcx_interp = tcx_name(f, testdir, '_interp', '.tcx')
+        args = ['./mpowertcx.py', '--csv', csv_full, '--tcx', tcx_interp, '--time', timestamp, '--interpolate']
         print ('run_samples.py: %r' % args)
         subprocess.call(args)
